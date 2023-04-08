f899b63d13fe8ddc8da4501fe14c57dc57a79dea
Carlos_Vinicius2340 <cvs.silva@discente.ufma.br>
2023-04-08 15:59:51 -0300

    find category by id, exception handling

--- src/main/java/com/solacsuicini/catalog/resources/CategoryResource.java
+++ src/main/java/com/solacsuicini/catalog/resources/CategoryResource.java
@@ -6,12 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.solacsuicini.catalog.entities.Category;
 import com.solacsuicini.catalog.services.CategoryService;
+import com.solacsuicini.catalog.services.exceptions.EntityNotFoundException;
 
 import dto.CategoryDTO;
 
@@ -26,5 +28,13 @@ public class CategoryResource {
   public ResponseEntity<List<CategoryDTO>> findAll() {
     List<CategoryDTO> list = service.findAll();
     return ResponseEntity.ok().body(list);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<CategoryDTO> findByID(@PathVariable Long id) {
+
+    CategoryDTO dto = service.findById(id);
+    return ResponseEntity.ok().body(dto);
+  
   }
 }
--- src/main/java/com/solacsuicini/catalog/services/CategoryService.java
+++ src/main/java/com/solacsuicini/catalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.solacsuicini.catalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.solacsuicini.catalog.entities.Category;
 import com.solacsuicini.catalog.repositories.CategoryRepository;
+import com.solacsuicini.catalog.services.exceptions.EntityNotFoundException;
 
 import dto.CategoryDTO;
 
@@ -22,9 +24,15 @@ public class CategoryService {
   public List<CategoryDTO> findAll() {
     List<Category> list = repository.findAll();
     List<CategoryDTO> listDTO = list.stream().map(cat -> new CategoryDTO(cat)).collect(Collectors.toList());
-
     return listDTO;
-    
+  }
+
+  @Transactional(readOnly=true)
+  public CategoryDTO findById(Long id) {
+    Optional<Category> optionalCat = repository.findById(id);
+    Category entityCat = optionalCat.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+    return new CategoryDTO(entityCat);
+
   }
    
 }
